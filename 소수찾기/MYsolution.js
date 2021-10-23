cb9356cef89db8079021e90252e4134b67ac328a
godyuo <godyuo@gmail.com>
2021-10-23 16:57:15 +0900

    소수찾기 시간복잡도 해결

--- 소수찾기/MYsolution.js
+++ 소수찾기/MYsolution.js
@@ -1,21 +1,41 @@
 function solution(n) {
-    var answer = 0;
-    let sum = [2];
-
-    for(let i = 3; i <= n; i += 2){
-        let a = true;
-        let cal = Math.floor(Math.sqrt(i))
-        for(let j = 2; j <= cal; j ++){
-            if(i % j === 0){
-                a = false;
-            break;
+    //     처음 푼것
+    //     var answer = 0;
+    //     let sum = [2];
+        
+        
+    //     for(let i = 3; i <= n; i += 2){
+    //         sum.push(i)
+    //     }
+        
+    //     function add(array) {
+    //         if(Math.pow(array[0], 2) >= n){
+    //             answer = array.length
+    //             return;
+    //         }
+    //         else{
+    //             let result = [];
+    //             result = array.filter(el => el % array[0] !== 0)
+    //             result.push(array[0])
+    //             add(result)
+    //         }
+    //     }
+    //     add(sum)
+    //     return answer
+        var answer = 0;
+        let arr = [];
+    
+        for(let i = 2; i <= n; i ++){
+             arr[i] = i
+         }
+        for(let i = 2; i <= n; i ++){
+            if( arr[i] === 0 ){ continue; }
+            
+            for(let j = i + i; j <= n; j += i){
+                if(arr[j] === 0){ continue; }
+                else { arr[j] = 0 }
             }
         }
-        if(a){
-            sum.push(i)
-        }
+        answer = arr.filter((el) => el !== 0)
+        return answer.length
     }
-    console.log(sum)
-    answer = sum.length;
-    return answer;
-}
